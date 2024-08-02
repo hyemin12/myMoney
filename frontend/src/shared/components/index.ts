937f1ae104054f6e1d5347ddf6c0fddd46667156
hyemin12 <khnm31@naver.com>
2024-08-02 19:57:08 +0900

    ect: 변경사항 추가하지 않았던 파일들 커밋

--- frontend/src/shared/components/index.ts
+++ frontend/src/shared/components/index.ts
@@ -1,11 +1,10 @@
 export { default as AlertText } from './AlertText';
 export { default as Button } from './Button';
-export { default as Category } from '../../features/category/components/Category';
-export { default as CategoryButton } from '../../features/category/components/CategoryButton';
 export { default as Dropdown } from './Dropdown';
 export { default as Icon } from './Icon';
 export { default as ImageSlide } from './ImageSlide';
 export { default as Input } from './Input';
 export { default as Loading } from './Loading';
-export { default as Modal } from './Modal';
+export { default as Modal } from './Modal/Modal';
 export { default as Badge } from './Badge';
+export { default as ReviewActions } from './ReviewActions';
