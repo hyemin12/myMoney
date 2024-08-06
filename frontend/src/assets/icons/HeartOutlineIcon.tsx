21d1b1fee2093271665aa066b8fd68d795824295
hyemin12 <khnm31@naver.com>
2024-08-06 12:11:42 +0900

    fix: 하트 아이콘 오류 수정 (Fill이 제대로 되지 않음, outLine 하트에 stroke 스타일 중복)

--- frontend/src/assets/icons/HeartOutlineIcon.tsx
+++ frontend/src/assets/icons/HeartOutlineIcon.tsx
@@ -5,7 +5,6 @@ export default function HeartIcon() {
       height="32"
       viewBox="0 0 32 32"
       fill="none"
-      stroke="#4F5458"
       xmlns="http://www.w3.org/2000/svg"
     >
       <path
