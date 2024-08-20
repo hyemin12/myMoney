38a2d4662cad4d6aa1a8c383b2904ca35ddccf0f
hyemin12 <khnm31@naver.com>
2024-08-20 15:25:14 +0900

    refactor: 코드 분할(lazy, suspense 사용)

--- frontend/src/Router.tsx
+++ frontend/src/Router.tsx
@@ -1,69 +1,69 @@
+import { lazy, Suspense } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-import Home from '@/pages/Home';
-import Login from '@/pages/Login';
-import Search from '@/pages/Search';
-
-import JoinStep1Email from '@/pages/Join/JoinStep1Email';
-import JoinStep2Nickname from '@/pages/Join/JoinStep2Nickname';
-import JoinStep3Password from '@/pages/Join/JoinStep3Password';
-
-import CreateReview from '@/pages/Review/CreateReview';
-import ReviewList from '@/pages/Review/ReviewList';
-import ReviewDetail from '@/pages/Review/ReviewDetail';
-import EditReview from '@/pages/Review/EditReview';
-
-import MyPage from '@/pages/Mypage/MyPage';
-import MyReviews from '@/pages/Mypage/MyReviews';
-import LikedReviews from '@/pages/Mypage/LikedReviews';
-
-import AdminLogin from '@/pages/Admin/AdminLogin';
-import ReportedUsersDashboard from '@/pages/Admin/ReportedUsersDashboard';
-import UnverifiedReviewsDashboard from '@/pages/Admin/UnverifiedReviewsDashboard';
-import { NotFoundError } from './features/error';
-import UserManagementDashboard from './pages/Admin/UserManagementDashboard';
+const Home = lazy(() => import('@/pages/Home'));
+const Search = lazy(() => import('@/pages/Search'));
+const CreateReview = lazy(() => import('@/pages/Review/CreateReview'));
+const EditReview = lazy(() => import('@/pages/Review/EditReview'));
+const ReviewList = lazy(() => import('@/pages/Review/ReviewList'));
+const ReviewDetail = lazy(() => import('@/pages/Review/ReviewDetail'));
+const MyPage = lazy(() => import('@/pages/Mypage/MyPage'));
+const Login = lazy(() => import('@/pages/Login'));
+const ReportedUsersDashboard = lazy(
+  () => import('@/pages/Admin/ReportedUsersDashboard'),
+);
+const UserManagementDashboard = lazy(
+  () => import('@/pages/Admin/UserManagementDashboard'),
+);
+const UnverifiedReviewsDashboard = lazy(
+  () => import('@/pages/Admin/UnverifiedReviewsDashboard'),
+);
+const JoinStep1Email = lazy(() => import('@/pages/Join/JoinStep1Email'));
+const JoinStep2Nickname = lazy(() => import('@/pages/Join/JoinStep2Nickname'));
+const JoinStep3Password = lazy(() => import('@/pages/Join/JoinStep3Password'));
+const MyReviews = lazy(() => import('@/pages/Mypage/MyReviews'));
+const LikedReviews = lazy(() => import('@/pages/Mypage/LikedReviews'));
 
 export function Router() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/search" element={<Search />} />
-        <Route path="/review" element={<CreateReview />} />
-        <Route path="/review/:id" element={<EditReview />} />
-        <Route path="/list" element={<ReviewList />} />
-        <Route path="/list/:id" element={<ReviewDetail />} />
-        <Route path="/mypage" element={<MyPage />} />
-        <Route path="/login" element={<Login />} />
+      <Suspense fallback={<div>로딩중..</div>}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/review" element={<CreateReview />} />
+          <Route path="/review/:id" element={<EditReview />} />
+          <Route path="/list" element={<ReviewList />} />
+          <Route path="/list/:id" element={<ReviewDetail />} />
+          <Route path="/mypage" element={<MyPage />} />
+          <Route path="/login" element={<Login />} />
 
-        {/* 관리자 페이지 */}
-        <Route path="/admin">
-          <Route index element={<AdminLogin />} />
-          <Route path="report-user" element={<ReportedUsersDashboard />} />
-          <Route path="users" element={<UserManagementDashboard />} />
-          <Route
-            path="unverified-reviews"
-            element={<UnverifiedReviewsDashboard />}
-          />
-        </Route>
+          {/* 관리자 페이지 */}
+          <Route path="/admin">
+            <Route path="report-user" element={<ReportedUsersDashboard />} />
+            <Route path="users" element={<UserManagementDashboard />} />
+            <Route
+              path="unverified-reviews"
+              element={<UnverifiedReviewsDashboard />}
+            />
+          </Route>
 
-        {/* 회원가입 */}
-        <Route path="/join">
-          <Route index element={<JoinStep1Email />} />
-          <Route path="step1" element={<JoinStep1Email />} />
-          <Route path="step2" element={<JoinStep2Nickname />} />
-          <Route path="step3" element={<JoinStep3Password />} />
-        </Route>
+          {/* 회원가입 */}
+          <Route path="/join">
+            <Route index element={<JoinStep1Email />} />
+            <Route path="step1" element={<JoinStep1Email />} />
+            <Route path="step2" element={<JoinStep2Nickname />} />
+            <Route path="step3" element={<JoinStep3Password />} />
+          </Route>
 
-        {/* 마이페이지 */}
-        <Route path="/mypage">
-          <Route index element={<MyPage />} />
-          <Route path="reviews" element={<MyReviews />} />
-          <Route path="liked" element={<LikedReviews />} />
-        </Route>
-
-        <Route path="*" element={<NotFoundError />} />
-      </Routes>
+          {/* 마이페이지 */}
+          <Route path="/mypage">
+            <Route index element={<MyPage />} />
+            <Route path="reviews" element={<MyReviews />} />
+            <Route path="liked" element={<LikedReviews />} />
+          </Route>
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 }
